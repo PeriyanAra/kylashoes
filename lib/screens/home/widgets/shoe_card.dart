f00e8493fc7eed734ac36671599927178c45eb05
Ara Periyan <ara.periyan-extern@invia.de>
2023-01-21 15:47:09 +0400

    add item info screen navigation

--- lib/screens/home/widgets/shoe_card.dart
+++ lib/screens/home/widgets/shoe_card.dart
@@ -1,5 +1,7 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:kylashoes/common/widgets/image_shadow.dart';
+import 'package:kylashoes/router/app_router.dart';
 import 'package:kylashoes/screens/home/widgets/shoe_card_main_info.dart';
 import 'package:kylashoes/view_models/shoe_view_model.dart';
 
@@ -27,52 +29,57 @@ class ShoeCard extends StatelessWidget {
 
     return AnimatedBuilder(
       animation: animation,
-      builder: (context, child) => Stack(
-        clipBehavior: Clip.none,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(
-              right: isPrevious
-                  ? animation.value
-                  : 20 + (100 * personalAngle).abs(),
-              left: 20 + (100 * personalAngle).abs(),
-              top: 100 - scale * 25,
-              bottom: 100 - scale * 25,
-            ),
-            child: Transform(
-              transform: Matrix4.identity()
-                ..setEntry(
-                  3,
-                  2,
-                  0.002,
-                )
-                ..rotateY(
-                  personalAngle,
-                ),
-              alignment: Alignment.center,
-              child: ShoeCardMainInfo(
-                shoeViewModel: shoeViewModel,
+      builder: (context, child) => GestureDetector(
+        onTap: () {
+          context.router.push(const ItemInfoRoute());
+        },
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(
+                right: isPrevious
+                    ? animation.value
+                    : 20 + (100 * personalAngle).abs(),
+                left: 20 + (100 * personalAngle).abs(),
+                top: 100 - scale * 25,
+                bottom: 100 - scale * 25,
               ),
-            ),
-          ),
-          Positioned(
-            right: isPrevious ? (animation.value - 21) * 2 : -21,
-            top: 80,
-            child: Transform.rotate(
-              angle: -angle,
-              alignment: const Alignment(0.5, -0.5),
-              child: ImageShadow(
-                offset: const Offset(14, 10),
-                sigma: 20,
-                opacity: 0.35,
-                child: Image.asset(
-                  shoeViewModel.imagePath,
-                  width: 250,
+              child: Transform(
+                transform: Matrix4.identity()
+                  ..setEntry(
+                    3,
+                    2,
+                    0.002,
+                  )
+                  ..rotateY(
+                    personalAngle,
+                  ),
+                alignment: Alignment.center,
+                child: ShoeCardMainInfo(
+                  shoeViewModel: shoeViewModel,
                 ),
               ),
             ),
-          ),
-        ],
+            Positioned(
+              right: isPrevious ? (animation.value - 21) * 2 : -21,
+              top: 80,
+              child: Transform.rotate(
+                angle: -angle,
+                alignment: const Alignment(0.5, -0.5),
+                child: ImageShadow(
+                  offset: const Offset(14, 10),
+                  sigma: 20,
+                  opacity: 0.35,
+                  child: Image.asset(
+                    shoeViewModel.imagePath,
+                    width: 250,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
