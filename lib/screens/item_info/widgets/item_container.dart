6335ac1e4fc2a1a4551e8daa03d38defad9f0804
Ara Periyan <periyan.ara@gmail.com>
2023-01-21 16:02:56 +0400

    merge

--- lib/screens/item_info/widgets/item_container.dart
+++ lib/screens/item_info/widgets/item_container.dart
@@ -35,39 +35,5 @@ class ItemContainer extends StatelessWidget {
         ),
       ],
     );
-
-    //    Stack(
-    //     alignment: AlignmentDirectional.center,
-    //     children: [
-    //       // Positioned(
-    //       //   child: Container(
-    //       //     height: 350,
-    //       //     decoration: const BoxDecoration(
-    //       //       color: Color.fromRGBO(223, 162, 144, 1),
-    //       //       borderRadius: BorderRadius.only(
-    //       //         bottomRight: Radius.circular(0),
-    //       //         bottomLeft: Radius.circular(200),
-    //       //       ),
-    //       //     ),
-    //       //   ),
-    //       // ),
-
-    //       Positioned(
-    //         left: 50,
-    //         bottom: 20,
-    //         child: Container(
-    //           // width: 1000,
-    //           // height: MediaQuery.of(context).size.height / 2.2,
-    //           decoration: BoxDecoration(
-    //             color: Colors.red,
-    //             borderRadius: BorderRadius.only(
-    //               bottomLeft: Radius.circular(1500),
-    //               bottomRight: Radius.circular(100),
-    //             ),
-    //           ),
-    //         ),
-    //       ),
-    //     ],
-    //   );
   }
 }
