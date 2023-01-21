e34f903f31acdd3acd753dd7395e0e6f1686679a
Ara Periyan <ara.periyan-extern@invia.de>
2023-01-21 17:05:11 +0400

    top part change

--- lib/screens/item_info/widgets/item_container.dart
+++ lib/screens/item_info/widgets/item_container.dart
@@ -43,20 +43,3 @@ class ItemContainer extends StatelessWidget {
     );
   }
 }
-
-class CustumPath extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    final path = Path();
-    path.lineTo(0, size.height);
-    path.lineTo(size.width, size.height);
-    path.lineTo(size.width, 0);
-    path.lineTo(size.width, 0);
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return true;
-  }
-}
