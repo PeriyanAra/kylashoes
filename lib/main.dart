45512d52170c72598c2c8aa9ad1ba79f647a1859
Ara Periyan <periyan.ara@gmail.com>
2023-01-22 14:22:17 +0400

    remove auto route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:kylashoes/router/app_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kylashoes/bloc/shoe_bloc.dart';
+import 'package:kylashoes/screens/bag/bag_screen.dart';
+import 'package:kylashoes/screens/favorite/favorite_screen.dart';
+import 'package:kylashoes/screens/home/home_screen.dart';
+import 'package:kylashoes/screens/profile/profile_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,14 +22,51 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  final _appRouter = AppRouter();
+  int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerDelegate: _appRouter.delegate(),
-      routeInformationParser: _appRouter.defaultRouteParser(),
-      debugShowCheckedModeBanner: false,
+    return BlocProvider(
+      create: (context) => ShoeBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          bottomNavigationBar: NavigationBar(
+            onDestinationSelected: (int index) {
+              setState(() {
+                currentPageIndex = index;
+              });
+            },
+            selectedIndex: currentPageIndex,
+            destinations: const <Widget>[
+              NavigationDestination(
+                icon: Icon(Icons.explore),
+                label: 'Explore',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.commute),
+                label: 'Commute',
+              ),
+              NavigationDestination(
+                selectedIcon: Icon(Icons.bookmark),
+                icon: Icon(Icons.bookmark_border),
+                label: 'Saved',
+              ),
+              NavigationDestination(
+                selectedIcon: Icon(Icons.bookmark),
+                icon: Icon(Icons.bookmark_border),
+                label: 'Saved',
+              ),
+            ],
+          ),
+          body: const <Widget>[
+            HomeScreen(),
+            FavoriteScreen(),
+            BagScreen(),
+            ProfileScreen(),
+          ][currentPageIndex],
+        ),
+      ),
     );
   }
 }
